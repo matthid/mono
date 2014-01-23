3833f22bd31f5c717d38663402a06b1bc05ae291
Matthias Dittrich <matthi.d@googlemail.com>
2014-01-23 16:08:29 +0100

    Changes to make the nunit tests run on my windows machine...

---
+++ mcs/class/corlib/Test/OldNUnitWorkaround.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#if !MOBILE
+namespace NUnit.Framework.SyntaxHelpers {
+}
+#endif
